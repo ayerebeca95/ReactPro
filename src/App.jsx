985bb74601125195bc5483ba1ea672e54a021091
ayerebeca95 <ayerebeca95@gmail.com>
2022-06-29 01:29:31 -0400

    Agregue boton contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import "./index.css";
+import { useState } from "react";
+//import { Accordion } from "@mui/material";
+import "./App.css";
 //import Input from "./components/Input";
+//import AcordionControlado from "./components/AcordionControlado";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./containers/ItemListContainer";
+import ItemCount from "./ItemCount/ItemCount.js";
 
 function App() {
+  const [containerVisible, setContainerVisible] = useState (true)
+  
+  const onHideContainer = () => {
+    setContainerVisible (false)
+  }
+
   return (
     <div className="container">
       <NavBar />
-      <ItemListContainer greeting= "Hola coders" />
+      <ItemCount/>
+      {containerVisible ? 
+      <ItemListContainer greeting= "Hola coders">
+        <h2> Este es el item list container</h2>
+      </ItemListContainer>
+      :
+      null
+    }
+    <button onClick={onHideContainer} >Desmontarl item list</button>
 </div>
   );
 }
--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import jacuzzi1 from "../../assets/jacuzzi1.jpg";
+import React, { useState, useEffect } from "react";
 
-const ItemListContainer = ({greeting}) => {
-  return (
-    <div>
-   <p> {greeting} </p>
-   <img src= {jacuzzi1}  alt="jacuzzi" />
-   </div>
+
+//import jacuzzi1 from "../../assets/jacuzzi1.jpg";
+
+const ItemListContainer = ({ greeting, children }) => {
+ // let color = "blue";
+
+ const [color, setColor] = useState ("blue");
+
+  const cambiarColor = () => {
+  if (color === "blue" ) setColor ("red")
+  else setColor ("blue")
+  }
+   
+  useEffect (() => {
+    console.log("se monto un componente")
+  }, [color]
   )
-}
 
-export default ItemListContainer
+  useEffect ( () => {
+    return () => {
+      console.log("se desmonta");
+    }
+  }, [])
+
+  return (
+    <div style= {{backgroundColor : color}}>
+      <p> {greeting} </p>
+      <p> {color} </p>
+      <button onClick={cambiarColor}>
+        Cambiar color a red
+      </button>
+
+      {/* <img src= {jacuzzi1}  alt="jacuzzi" />*/}
+      {children}
+      <button className="btn btn-primary lg"> Presione aqui </button>
+    </div>
+  );
+};
+
+export default ItemListContainer;
